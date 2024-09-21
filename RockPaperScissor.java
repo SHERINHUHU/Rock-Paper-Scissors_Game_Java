b5b4178c013f10b7d0a8137cccae8bc4794bfc31
SHERINHUHU <preemoon76@gmail.com>
2024-09-21 11:38:55 +0800

    Update RockPaperScissors2.java

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -1,13 +1,20 @@
-import java.util.Scanner;
+package rockpaperscissors2;
+
+
 
 import static java.lang.Math.random;
+import java.util.Scanner;
 
 
 
-public class RockPaperScissor {
+public class RockPaperScissors2 {
+   
+
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
+
+        
         String[] choices = new String[3];
         choices[0] = "paper";
         choices[1] = "scissors";
@@ -26,12 +33,68 @@ public class RockPaperScissor {
                 if(i == rand) continue;
                 if(compareStrings(input,choices[i])) idx = i;
             }
-            if(rand-idx==-1||rand-idx==2) System.out.println("The player wins");
-            else if (rand == idx) {System.out.println("Tie"); again = true;}
-            else System.out.println("The Computer wins");
-        } while (again);
+            if(rand-idx==-1||rand-idx==2)
+            { 
+                System.out.println("The player wins");
+            int a = (int) (Math.random()*10);
+            int b = (int) (Math.random()*10);
+           int sum = Math.addExact(a , b);
+           int total = 0;
+             int jum = Math.addExact(total , sum) ;
+     total = jum ;
+
+            System.out.println (" Congratulation,you have won " + total + " coins! ");
+}
+            //generate 2 random number as the coin rewards
+           
+            if (rand-idx==-1||rand-idx==2){
+            
+            }
+           
+            else if (rand == idx) 
+                    {
+System.out.println("Tie"); again = true;
+                    }
+            else {System.out.println("The Computer wins");
+          
+                      System.out.println (" Oh no ! Try again.");
+                      
+                      System.out.println (" continue playing?");
+                      String[] choose = new String[2];
+        //create an array
+        choose[0] = "yes";
+        choose[1] = "no";
+System.out.println (choose[0]);
+System.out.println (choose[1]);
+   
+ String pilih = in.nextLine();
+ 
+                 
+        
+    if (    "yes".equals(pilih))
+    { 
+System.out.println ("Let's play again");
     }
-    public static boolean compareStrings(String a, String b) {
+    else
+    {
+              System.exit(0);
+    }
+            }
+            
+                      
+                     
+                        
+                           
+
+                 
+            
+                       
+            
+         } while (again);
+    }
+
+   
+    public static boolean compareStrings (String a, String b) {
         int sz1 = a.length(), sz2 = b.length();
         if(sz2!=sz1) return false;
         for (int i = 0; i < sz1; i++) {
